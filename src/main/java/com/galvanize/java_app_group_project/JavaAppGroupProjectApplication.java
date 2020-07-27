d446ecb3b4c79fdf99cb3bb43d1279da92bde890
AlejandroBocanegra <a.boca@outlook.com>
2020-07-27 23:44:32 +0900

    initial push

---
+++ src/main/java/com/galvanize/java_app_group_project/JavaAppGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.java_app_group_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppGroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/java_app_group_project/JavaAppGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.java_app_group_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
