ffbb05ecff3b2352bf87a7560158694aaebdda80
AlejandroBocanegra <a.boca@outlook.com>
2020-07-30 01:09:58 +0900

    cleaning up dead code

--- src/main/java/com/galvanize/java_app_group_project/MedicalReadinessController.java
+++ src/main/java/com/galvanize/java_app_group_project/MedicalReadinessController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MedicalReadinessController {
